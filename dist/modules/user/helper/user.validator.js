8c1b08c27c6b54624ccab8502beb99aee74b78e0
readul-islam <developer.readul@gmail.com>
2023-11-23 15:18:07 +0600

    joi validator setup

--- dist/modules/user/helper/user.validator.js
+++ dist/modules/user/helper/user.validator.js
@@ -4,5 +4,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const joi_1 = __importDefault(require("joi"));
-const userValidator = joi_1.default.object({});
+const userNameValidator = joi_1.default.object({
+    firstName: joi_1.default.string().required().trim().max(20).regex(/^[A-Z][a-z]*$/, { name: 'capitalize' }),
+    lastName: joi_1.default.string().required().trim(),
+});
+const userAddressValidator = joi_1.default.object({
+    street: joi_1.default.string().required().trim(),
+    city: joi_1.default.string().required().trim(),
+    country: joi_1.default.string().required().trim(),
+});
+const userOrdersValidator = joi_1.default.object({
+    productName: joi_1.default.string().required(),
+    price: joi_1.default.number().required(),
+    quantity: joi_1.default.number().required(),
+});
+const userValidator = joi_1.default.object({
+    userId: joi_1.default.number(),
+    username: joi_1.default.string().required().trim(),
+    password: joi_1.default.string().required().trim(),
+    fullName: userNameValidator.required(),
+    age: joi_1.default.number().required(),
+    email: joi_1.default.string().required().trim(),
+    isActive: joi_1.default.boolean().required(),
+    hobbies: joi_1.default.array().items(joi_1.default.string().required()).required(),
+    address: userAddressValidator.required(),
+    orders: userOrdersValidator.required(),
+});
 exports.default = userValidator;
--- src/modules/user/helper/user.validator.ts
+++ src/modules/user/helper/user.validator.ts
@@ -1,8 +1,31 @@
-import Joi from 'joi';
-const userValidator = Joi.object({
+import Joi from "joi";
 
+const userNameValidator = Joi.object({
+  firstName: Joi.string().required().trim().max(20).regex(/^[A-Z][a-z]*$/, { name: 'capitalize' }),
+  lastName: Joi.string().required().trim(),
+});
+const userAddressValidator = Joi.object({
+  street: Joi.string().required().trim(),
+  city: Joi.string().required().trim(),
+  country: Joi.string().required().trim(),
+});
+const userOrdersValidator = Joi.object({
+  productName: Joi.string().required(),
+  price: Joi.number().required(),
+  quantity: Joi.number().required(),
 });
 
-
+const userValidator = Joi.object({
+  userId: Joi.number(),
+  username: Joi.string().required().trim(),
+  password: Joi.string().required().trim(),
+  fullName: userNameValidator.required(),
+  age: Joi.number().required(),
+  email: Joi.string().required().trim(),
+  isActive: Joi.boolean().required(),
+  hobbies: Joi.array().items(Joi.string().required()).required(),
+  address: userAddressValidator.required(),
+  orders: userOrdersValidator.required(),
+});
 
 export default userValidator;
